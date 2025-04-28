7f297da79e53fc6416059f0354d90ae27bd86fad
Rayyan Ashraf <ryshashraf@gmail.com>
2025-04-29 04:31:54 +0530

    SPA-C

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ function App() {
           <Routes>
             {/* NLP */}
             <Route path={urlRoutes.natural_language_processing_dir.natural_language_processing} element={<NLP_root />} />
-            <Route path={urlRoutes.natural_language_processing_dir.phases_of_natural_language_processing_dir.phases_of_natural_language_processing} element={<NLP_PoNLP />} />
+            {/* <Route path={urlRoutes.natural_language_processing_dir.phases_of_natural_language_processing_dir.phases_of_natural_language_processing} element={<NLP_PoNLP />} /> */}
+            <Route path={"/f841e8e163c2b6d20d96f71eef66d18b3630c021dd54571a949983f29f589b54"} element={<NLP_PoNLP />} />
             
 
             {/* Data Science */}
