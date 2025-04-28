b9d938739780d040f961c0e594a1101bda5e1325
Rayyan Ashraf <ryshashraf@gmail.com>
2025-04-29 03:53:48 +0530

    SPA-C

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,10 @@ function App() {
           <Routes>
             {/* NLP */}
             <Route path={urlRoutes.natural_language_processing.natural_language_processing} element={<NLP />} />
-            {/* <Route path={urlRoutes.login} element={<LoginPage />} />
-            <Route path={urlRoutes.signup.personal} element={<SignUpPageOne />} />
-            <Route path={urlRoutes.signup.contact} element={<SignUpPageTwo />} />
-            <Route path={urlRoutes.signup.email_opt_verification} element={<SignUpPageThree />} /> */}
+            
 
             {/* Data Science */}
-            {/* <Route path="/login/student" element={<StudentLoginPage />} /> */}
-            {/* <Route path="/login/examiner" element={<ExaminerLoginPage />} /> */}
-            {/* <Route path="/login/student/registration/FS" element={<StudentRegistrationFS />} /> */}
-            {/* <Route path=02"/login/student/registration/SS" element={<StudentRegistrationSS />} /> */}
+
           </Routes>
         </Router>
   )
