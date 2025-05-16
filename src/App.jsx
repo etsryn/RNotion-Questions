2398a6920300667adb6b984dbcf7f4e63df5645d
Rayyan Ashraf <ryshashraf@gmail.com>
2025-05-16 18:20:18 +0530

    Private

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import urlRoutes from "./Constants/Routes/questions-route";
 import NLP_root from './Subjects/Natural-Language-Processing/Natural-Language-Processing-Q/ui_nlp_q';
 import NLP_PoNLP from './Subjects/Natural-Language-Processing/Phases-of-Natural-Language-Processing-Q/ui_ponlp_q';
+import ALHAAN from './Subjects/Alhaan/ui_alhaan_q';
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
             {/* NLP */}
             <Route path={urlRoutes.natural_language_processing_dir.natural_language_processing} element={<NLP_root participantName={participantName} />} />
             <Route path={urlRoutes.natural_language_processing_dir.phases_of_natural_language_processing_dir.phases_of_natural_language_processing} element={<NLP_PoNLP participantName={participantName} />} />
+            <Route path={urlRoutes.alhaan_dir.alhaan} element={<ALHAAN participantName={participantName} />} />
             
 
             {/* Data Science */}
